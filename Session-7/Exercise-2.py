df6381869868e6b56d5817f73069baac03d7744b
anlmgp <anitalopezmarotog@gmail.com>
2019-02-13 17:22:32 +0100

    Test 3 (connect with the teacher service) +

--- Session-7/Exercise-2.py
+++ Session-7/Exercise-2.py
@@ -6,12 +6,12 @@ while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print ("Socket created")
     PORT = 8080
-    IP = "212.128.253.113"
+    IP = "212.128.253.64"
     s.connect((IP, PORT))
     s.send(str.encode(input("Write a message:")))
     msg = s.recv(2048).decode('utf-8')
     print("MESSAGE FROM THE SERVICE")
     print(msg)
-
+    s.close()
 
 print("The end")

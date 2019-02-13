e629c6fa3ff586c273d9febacdd5f080ad77136c
anlmgp <anitalopezmarotog@gmail.com>
2019-02-13 17:24:13 +0100

    Example in class

--- Session-7/server.py
+++ Session-7/server.py
@@ -1,8 +1,9 @@
 import socket
 
 # Configure the Server's IP and PORT
-PORT = 8081
-IP = "212.128.253.100"
+PORT = 8081 #To connect with my own server i need to put 8081
+
+IP = "212.128.253.112"
 MAX_OPEN_REQUESTS = 5
 
 # Counting the number of connections

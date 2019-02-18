f8576e5e82dcadf1ed118f90bf864c540b7bfb18
anlmgp <anitalopezmarotog@gmail.com>
2019-02-18 18:12:52 +0100

    Test 2

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -1,1 +1,17 @@
-from  Seq import
+from Seq import Seq
+
+Sequence1 = Seq( "AGTACACTGGT")
+Sequence2 = Seq ("CGTAAC")
+Sequence3 = Sequence1.complement()
+Sequence4 = Sequence3.reverse()
+
+Sequences = [Sequence1, Sequence2, Sequence3, Sequence4]
+Bases = ['A','T','G','C']
+number = 0
+for i in Sequences:
+    number = number + 1
+    print('Sequence {}:'.format(number),i.strbases)
+    print('  Length: {}'.format(i.len()))
+    for s in Bases:
+        print('  Bases count: {}'.format(i.count(s)))
+        print('  Bases percentage: {}'.format())

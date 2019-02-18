c2366c06485daa98b7bf58277170eb8828776b0f
anlmgp <anitalopezmarotog@gmail.com>
2019-02-18 16:45:00 +0100

    Test 3

--- P1/Seq.py
+++ P1/Seq.py
@@ -6,12 +6,26 @@ class Seq:
     def len (self):
         return len(self.strbases)
 
-    def complement (self):
-
+    def complement(self):
+        bases = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+        result1 = []
+        for i in self.strbases:
+            if i in bases:
+                result1.append(bases[i])
+        result1 = "".join(result1)
+        return (result1)
 
     def reverse(self):
-        return self[::-1]
+        return self.strbases[::-1]
 
-    def count ()
+    def count(self):
+        bases = ['A', 'C', 'T', 'G']
+        for i in bases:
+            result2 = self.strbases.count(i)
+            print(result2)
 
-    def perc ()
+    def perc(self):
+        cb = self.count(s)
+        for i in self:
+            result3 = (round(100.0 * cb[i] / len(self), 1))
+            return (result3)

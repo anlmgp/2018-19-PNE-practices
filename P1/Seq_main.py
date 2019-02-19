0e564f63ef1f596c5084f4760279c1dc7deabf68
anlmgp <anitalopezmarotog@gmail.com>
2019-02-19 02:37:05 +0100

    Final Practice

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -7,11 +7,34 @@ Sequence4 = Sequence3.reverse()
 
 Sequences = [Sequence1, Sequence2, Sequence3, Sequence4]
 Bases = ['A','T','G','C']
+
 number = 0
 for i in Sequences:
     number = number + 1
     print('Sequence {}:'.format(number),i.strbases)
     print('  Length: {}'.format(i.len()))
+    counter = []
+    m = []
+    counter2 = []
+    h = []
     for s in Bases:
-        print('  Bases count: {}'.format(i.count(s)))
-        print('  Bases percentage: {}'.format(i.perc(s)))
+        r = i.count1(s)
+        counter.append(r)
+        result = dict(zip(Bases, counter))
+        for key, value in result.items():
+            s = str(key + ':' + str(value))
+        m.append(s)
+        result2 = ", ".join(m)
+    print('  Bases count: {}'.format(result2))
+    for s in Bases:
+        t = i.perc(s)
+        counter2.append(t)
+        resultt = dict(zip(Bases, counter2))
+        for key, value in resultt.items():
+            s = str(key + ':' + str(value))
+        h.append(s)
+        result4 = " %, ".join(h)
+
+    print('  Bases percentage: {}%'.format(result4))
+
+

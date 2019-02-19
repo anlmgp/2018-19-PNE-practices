0bd6be8bf6662b75659a0a8c84ba533b5e405f26
anlmgp <anitalopezmarotog@gmail.com>
2019-02-19 11:26:43 +0100

    Final exercise

--- P0/fibonacci_sum.py
+++ P0/fibonacci_sum.py
@@ -1,9 +1,11 @@
 def fibonacci (n):
-    a = 1
-    b = 0
+    a = 0
+    b = 1
+    counter = 0
     for i in range(n):
+        counter = counter + a
         a,b = b, a + b
-    return a
+    return counter
 
 n = int(input('Select the number of the sequence:'))
-print (fibonacci(n))
+print ('The sum of the first',n,'numbers in the fibonnaci serie is',fibonacci(n))

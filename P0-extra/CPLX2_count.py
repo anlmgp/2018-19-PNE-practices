94277145c84c4a869c39518536b028e66201ef8e
anlmgp <anitalopezmarotog@gmail.com>
2019-02-21 18:19:37 +0100

    Test 3

--- P0-extra/CPLX2_count.py
+++ P0-extra/CPLX2_count.py
@@ -7,16 +7,17 @@ count_T = 0
 count_G = 0
 
 for i in CPLX2:
-    if CPLX2.startswith('>'):
-        continue
-    elif i == 'A':
-        count_A = count_A + 1
-    elif i == 'C':
-        count_C = count_C + 1
-    elif i == 'T':
-        count_T = count_T + 1
-    elif i == 'G':
-        count_G = count_G +1
+    if i.startswith('>'):
+       continue
+    else:
+        if i == 'A':
+            count_A = count_A + 1
+        elif i == 'C':
+            count_C = count_C + 1
+        elif i == 'T':
+            count_T = count_T + 1
+        elif i == 'G':
+            count_G = count_G +1
 
 print('A:', count_A)
 print('C:', count_C)

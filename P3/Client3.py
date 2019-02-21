5715b7bc08f590abefcb6a5a652be6c55e9fbcd3
anlmgp <anitalopezmarotog@gmail.com>
2019-02-21 19:02:38 +0100

    NOt finish

---
+++ P3/Client3.py
@@ -0,0 +1,29 @@
+import socket
+
+IP = "212.128.253.95"
+PORT = 8884
+
+while True:
+
+    # Before connecting to the server, ask the user for the string
+    msg = input("> ")
+    if len(msg) == 0:
+        msg = " "
+
+
+    # Now we can create the socket and connect to the servewr
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # establish the connection to the Server (IP, PORT)
+    s.connect((IP, PORT))
+
+    # Send the request message to the server
+    s.send(str.encode(msg))
+
+    # Receive the servers respoinse
+    response = s.recv(2048).decode()
+
+    # Print the server's response
+    print("Response: {}".format(response))
+
+    s.close()

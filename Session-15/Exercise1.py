c97ed71e5f86c0113c26872c98b87b5ca259d7d9
anlmgp <anitalopezmarotog@gmail.com>
2019-03-26 16:52:25 +0100

    WORK!

--- Session-15/Exercise1.py
+++ Session-15/Exercise1.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 import termcolor
 
-PORT = 4000
+PORT = 4006
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
@@ -12,43 +12,54 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         termcolor.cprint(self.requestline, 'green')
         print (self.path)
 
-        f = open("form1.html", 'r')
-        contents = f.read()
-        self.path.lstrip('/echo?msg=')
-        if self.path == '/echo' in range[0:4]:
+        message = self.path.lstrip('/echo?msg=')
+        print (message)
+
+
+        if self.path.rstrip(message) == '/echo?msg=':
             file = """<!DOCTYPE html>
-            <html lang = "en">
-            < head>
-                <meta charset = "UTF-8">
-                <title> Message </ title>
-            </ head>
-            <body style = "background-color: white;" >
-              < h1 > Eco of the message recived < / h1 >
-              < p > self.path.lstrip('/echo?msg=') < / p >
-              < a href = "/" > Main page < / a >
-            < / body >
-            < / html >"""
-            contents = content.write(file)
-            content.close()
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <title>Title</title>
+                    </head>
+                    <body>
+                     <h1>Echo from received message</h1>
+                      <p>{}</p>
+                      <a href="/">Main page</a>
+                    </body>
+                    </html>""".format(message)
+
+            self.send_response(200)
+
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Lenght', len(str.encode(file)))
+            self.end_headers()
+
+        # -- SEnding the body of the response message
+            self.wfile.write(str.encode(file))
 
 
-        else:
+        elif self.path == '/' or self.path == '/favicon.ico':
             f = open("form1.html", 'r')
             contents = f.read()
-
-        self.send_response(200)
-
-        self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Lenght', len(str.encode(contents)))
-        self.end_headers()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Lenght', len(str.encode(contents)))
+            self.end_headers()
 
         #-- SEnding the body of the response message
-        self.wfile.write(str.encode(contents))
+            self.wfile.write(str.encode(contents))
 
-
-
-
-
+        else:
+            s = open("error.html")
+            contents = s.read()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Lenght', len(str.encode(contents)))
+            self.end_headers()
+            # -- SEnding the body of the response message
+            self.wfile.write(str.encode(contents))
 
 # -- Main program
 with socketserver.TCPServer(("", PORT), TestHandler) as httpd:

7a046950a1fa0e684ab6b2770f085f977b53bb4c
anlmgp <anitalopezmarotog@gmail.com>
2019-03-27 14:44:43 +0100

    Work

---
+++ Session-15/Exercise2.py
@@ -0,0 +1,96 @@
+import http.server
+import socketserver
+import termcolor
+
+PORT = 4006
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        #-- printing the request line
+        termcolor.cprint(self.requestline, 'green')
+        print (self.path)
+
+        message = self.path.replace('/echo?msg=', '').replace('&cap=on', '')
+        print (message)
+
+        path = self.path
+
+        if path.startswith('/echo?msg='):
+            if path.endswith ('&cap=on'):
+                message = message.upper()
+                file = """<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Title</title>
+                        </head>
+                        <body>
+                          <h1>Echo from received message</h1>
+                          <p>{}</p>
+                          <a href="/">Main page</a>
+                        </body>
+                        </html>""".format(message)
+
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/html')
+                self.send_header('Content-Lenght', len(str.encode(file)))
+                self.end_headers()
+                # -- SEnding the body of the response message
+                self.wfile.write(str.encode(file))
+
+            elif path.endswith(message):
+                file = """<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Title</title>
+                        </head>
+                        <body>
+                          <h1>Echo from received message</h1>
+                          <p>{}</p>
+                          <a href="/">Main page</a>
+                        </body>
+                        </html>""".format(message)
+
+                self.send_response(200)
+
+                self.send_header('Content-Type', 'text/html')
+                self.send_header('Content-Lenght', len(str.encode(file)))
+                self.end_headers()
+                # -- SEnding the body of the response message
+                self.wfile.write(str.encode(file))
+
+
+        elif self.path == '/' or self.path == '/favicon.ico':
+            f = open("form-exercise2.html", 'r')
+            contents = f.read()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Lenght', len(str.encode(contents)))
+            self.end_headers()
+
+        #-- SEnding the body of the response message
+            self.wfile.write(str.encode(contents))
+
+        else:
+            s = open("error.html")
+            contents = s.read()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Lenght', len(str.encode(contents)))
+            self.end_headers()
+            # -- SEnding the body of the response message
+            self.wfile.write(str.encode(contents))
+
+# -- Main program
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+    print("Serving at PORT: {}".format(PORT))
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+print("The server is closed")
